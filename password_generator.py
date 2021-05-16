8c89452857651e8c3ea3d10ebaf6ac2f2fa4710c
bratipah <bratipah@gmail.com>
2021-05-16 21:58:58 +0300

    add password code

--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,18 @@
+import random
+
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@!$,.(')"
+
+while 1:
+    password_length = 15
+    password_count = int(input("Enter how many passwords you would like: "))
+    
+    for x in range(0,password_count):
+        password = ""
+        for x in range(0,password_length):
+            password_char = random.choice(chars)
+            password      = password + password_char
+    print("Your password is: ",password)
+    break
+    
 
